d2345675226ec70d57e4982d0fb966bb65af07f3
Rmercad1 <113056985+Rmercad1@users.noreply.github.com>
2023-04-27 20:53:04 +0000

    la pampa

---
+++ scrapers/wikitable/la_pampa_scrape.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+url = 'https://es.wikipedia.org/wiki/Elecciones_provinciales_de_La_Pampa_de_2023'
+
+# Specify header row index
+header_row = 0
+
+# Set decimal and thousands separators
+decimal = ','
+thousands = '.'
+
+tables = pd.read_html(url, header=header_row, decimal=decimal, thousands=thousands)
+
+# table on the page
+df = tables[10]
+
+# Rename columns
+df.columns = ['Date', 'Pollster', 'Sample', 'Frente de Todos', 'Juntos por el Cambio',
+             'Comunidad Organizada', 'Frente de Izquierda', 'Others',
+             'Blank', 'Undecided', 'Lead']
+
+# Filter out rows where Date is NaN
+df = df[df['Date'].notna()]
+
+# Drop duplicate header row by index
+dup_idx = df[df['Date'].str.startswith('Fecha')].index
+df = df.drop(dup_idx)
+
+# Save to CSV
+df.to_csv('la_pampa_polls.csv', index=False)
