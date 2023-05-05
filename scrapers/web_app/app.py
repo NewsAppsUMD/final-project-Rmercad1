005d3ee1a9229d43106314fc24edacefa6c8b956
Rmercad1 <113056985+Rmercad1@users.noreply.github.com>
2023-05-05 04:52:54 +0000

    color

--- scrapers/web_app/app.py
+++ scrapers/web_app/app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
+# Route for the home page
 @app.route('/')
 def home():
     return render_template('home.html')
+
+# Route for displaying the Spanish voting intentions table
+@app.route('/spanish_voting_intentions')
+def display_spanish_voting_intentions():
+    df = pd.read_csv('scrapers/wikitable/spanish_voting_intentions.csv')
+    return render_template('table.html', data=df.to_html())
 
 if __name__ == '__main__':
     app.run(debug=True)
