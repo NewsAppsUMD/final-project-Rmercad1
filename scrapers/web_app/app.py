acf9354556dc9fbc68bb78336135c1ab97426b1a
Rmercad1 <113056985+Rmercad1@users.noreply.github.com>
2023-05-18 02:36:28 +0000

    province pages

--- scrapers/web_app/app.py
+++ scrapers/web_app/app.py
@@ -25,5 +25,94 @@ def provinces():
 def display_corrientes():
     return render_template('corrientes.html')
 
+# Route for the San Juan province page
+@app.route('/provinces/sanjuan')
+def display_sanjuan():
+    return render_template('sanjuan.html')
+
+# Route for the Tucuman province page
+@app.route('/provinces/tucuman')
+def display_tucuman():
+    return render_template('tucuman.html')
+
+@app.route('/provinces/cordoba')
+def display_cordoba():
+    return render_template('cordoba.html')
+
+@app.route('/provinces/formosa')
+def display_formosa():
+    return render_template('formosa.html')
+
+@app.route('/provinces/chubut')
+def display_chubut():
+    return render_template('chubut.html')
+
+@app.route('/provinces/santacruz')
+def display_santacruz():
+    return render_template('santacruz.html')
+
+@app.route('/provinces/chaco')
+def display_chaco():
+    return render_template('chaco.html')
+
+@app.route('/provinces/mendoza')
+def display_mendoza():
+    return render_template('mendoza.html')
+
+@app.route('/provinces/caba')
+def display_caba():
+    return render_template('caba.html')
+
+@app.route('/provinces/catamarca')
+def display_catamarca():
+    return render_template('catamarca.html')
+
+@app.route('/provinces/entrerios')
+def display_entrerios():
+    return render_template('entrerios.html')
+
+@app.route('/provinces/buenosaires')
+def display_buenosaires():
+    return render_template('buenosaires.html')
+
+@app.route('/provinces/neuquen')
+def display_neuquen():
+    return render_template('neuquen.html')
+
+@app.route('/provinces/rionegro')
+def display_rionegro():
+    return render_template('rionegro.html')
+
+@app.route('/provinces/jujuy')
+def display_jujuy():
+    return render_template('jujuy.html')
+
+@app.route('/provinces/larioja')
+def display_larioja():
+    return render_template('larioja.html')
+
+@app.route('/provinces/misiones')
+def display_misiones():
+    return render_template('misiones.html')
+
+@app.route('/provinces/lapampa')
+def display_lapampa():
+    return render_template('lapampa.html')
+
+@app.route('/provinces/salta')
+def display_salta():
+    return render_template('salta.html')
+
+@app.route('/provinces/sanjuan')
+def display_sanjuan():
+    return render_template('sanjuan.html')
+
+@app.route('/provinces/tierradelfuego')
+def display_tierradelfuego():
+    return render_template('tierradelfuego.html')
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
